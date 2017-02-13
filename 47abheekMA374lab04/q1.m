4d466e88bab0106ed305dfd6c9c025d1697849de
Abheek Ghosh <abheekghosh.ag@gmail.com>
2017-02-13 10:51:08 +0530

    Lab 4 almost complete

--- 47abheekMA374lab04/q1.m
+++ 47abheekMA374lab04/q1.m
@@ -1,117 +1,169 @@
 function q1
 	clear; close all;
-	S0 = 100;
-	K = 100;
-	T = 1;
-	r = 0.08;
-	M = 100;
-	sig = 0.2;
 
-	% Set 1
-	fprintf('\n\nSet 2\n');
+	m = [0.1, 0.2, 0.15];
+	C = [0.005, -0.010, 0.004; -0.010, 0.040, -0.002; 0.004, -0.002, 0.023];
+	r = 0.1;
 
-	% Set 2
-	fprintf('\n\nSet 2\n');
-	q1_part(S0, K, T, r, M, sig, @U2, @D2, 'S(0)');
-	q1_part(S0, K, T, r, M, sig, @U2, @D2, 'K');
-	q1_part(S0, K, T, r, M, sig, @U2, @D2, 'r');
-	q1_part(S0, K, T, r, M, sig, @U2, @D2, 'sigma');
-	q1_part(S0, K - 5, T, r, M, sig, @U2, @D2, 'M');
-	q1_part(S0, K, T, r, M, sig, @U2, @D2, 'M');
-	q1_part(S0, K + 5, T, r, M, sig, @U2, @D2, 'M');
+	syms x;
+	[f(x), g(x)] = weights(x, m, C);
+	[h(x), w_m, mu_m, sig_m] = capm(x, m, C, r);
+	
+	q1_part1(g);
+	q1_part2(f, g);
+	q1_part3(f, g, 0.15);
+	q1_part4(f, g, 0.18);
+	q1_part5(f, g, h, w_m, mu_m, sig_m);
+
 end
 
-function q1_part(S0, K, T, r, M, sig, U, D, part)
-	n = 100;
-	
-	S0_ = ones(1, n) * S0;
-	K_ = ones(1, n) * K;
-	r_ = ones(1, n) * r;
-	sig_ = ones(1, n) * sig;
-	M_ = ones(1, n) * M;
+function [w, sig] = weights(x, m, C)
+	M = zeros(2, 2);
+	u = ones(size(m));
 
-	if strcmp(part, 'S(0)')
-		S0_ = S0 * (2/n:2/n:2);
-		X = S0_;
-	elseif strcmp(part, 'K')
-		K_ = K * (2/n:2/n:2);
-		X = K_;
-	elseif strcmp(part, 'r')
-		r_ = r * (10/n:10/n:10);
-		X = r_;
-	elseif strcmp(part, 'sigma')
-		sig_ = sig * (2/n:2/n:2);
-		X = sig_;
-	else
-		M_ = 1:5:n*5;
-		X = M_;
+	Cinv = pinv(C);
+	M(1, 1) = m * Cinv * m';
+	M(1, 2) = u * Cinv * m';
+	M(2, 1) = m * Cinv * u';
+	M(2, 2) = u * Cinv * u';
+
+	lamda = pinv(M) * [x; 1];
+
+	w = (lamda(1)*m + lamda(2)*u) * Cinv;
+
+	sig = (w*C*w')^0.5;
+end
+
+function [h, w, mu_m, sig_m] = capm(x, m, C, r)
+	u = ones(size(m));
+
+	Cinv = pinv(C);
+	w = (m - r*u) * Cinv;
+	w = w / sum(w);
+
+	mu_m = m * w';
+	sig_m = (w*C*w')^0.5;
+
+	h(x) = (mu_m - r)*x/sig_m + r;
+end
+
+
+function q1_part1(g)
+	mu = 0:0.001:0.2;
+	sig = zeros(size(mu));
+	for i = 1:length(sig)
+		sig(i) = g(mu(i));
 	end
 
-	fprintf('\n\nVarying %s\n', part);
-	V0_put = [];
-	V0_call = [];
+	plot(sig, mu);
+end
 
-	for i = 1:n
-		S0 = S0_(i);
-		K = K_(i);
-		r = r_(i);
-		sig = sig_(i);
-		m = M_(i);
+function q1_part2(f, g)
+	mu = 0:0.03:0.27;
 
-		dt = T/m;
-		u = U(r, sig, dt);
-		d = D(r, sig, dt);
+	fprintf('\n\nWeights\t\t\t\t\tReturn\t\tRisk\n')
+	for i = 1:10
+		w = f(mu(i));
+		sig = g(mu(i));
+		fprintf('[')
+		for j = 1:length(w)
+			fprintf('%f, ', double(w(j)));
+		end
+		fprintf(']\t%f\t%f\n', mu(i), double(sig));
+	end
+end
 
-		if d > exp(r*dt) or u < exp(r*dt)
-			fprintf('Arbritage opportunity.\n')
+function q1_part3(f, g, sig)
+	% m = [0, 0];
+
+	l = 0.15;
+	r = 0.2;
+	while (abs(l - r) > 1e-7)
+		m(1) = (l + r)/2;
+		if (g(m(1)) > sig)
+			r = m(1);
 		else
-			V0_put = [V0_put, valuation(@euro_put, u, d, r, dt, S0, K, m)];
-			V0_call = [V0_call, valuation(@euro_call, u, d, r, dt, S0, K, m)];
+			l = m(1);
 		end
 	end
 
-	fprintf('%s\t\tEuropean Call (initial price)\tEuropean Put (initial price)\n', part);
-	for i = 1:n
-		fprintf('%d\t\t%f\t\t\t%f\n', X(i), V0_call(i), V0_put(i));
+	l = 0;
+	r = 0.1;
+	while (abs(l - r) > 1e-7)
+		m(2) = (l + r)/2;
+		if (g(m(2)) > sig)
+			l = m(2);
+		else
+			r = m(2);
+		end
 	end
 
-	figure; plot(X, V0_call); title(['European Call price (at time 0) vs ', part]); xlabel(part); ylabel('European Call price (at time 0)');
-	figure; plot(X, V0_put); title(['European Put price (at time 0) vs ', part]); xlabel(part); ylabel('European Put price (at time 0)');
-end
-
-function [z] = euro_call(x, y)
-	z = max(x - y, 0);
-end
-
-function [z] = euro_put(x, y)
-	z = max(y - x, 0);
-end
-
-function [u] = U1(r, sig, dt)
-	u = exp(sig * dt^0.5);
-end
-
-function [d] = D1(r, sig, dt)
-	d = exp(-sig * dt^0.5);
-end
-
-function [u] = U2(r, sig, dt)
-	u = exp(sig * dt^0.5 + (r - sig^2/2)*dt);
-end
-
-function [d] = D2(r, sig, dt)
-	d = exp(-sig * dt^0.5 + (r - sig^2/2)*dt);
-end
-
-function [V0, V] = valuation(VT, u, d, r, dt, S0, K, m)
-	p = (exp(r*dt) - d) / (u - d);
-	q = 1 - p;
-	V = zeros(m + 1, m + 1);
-
-	V(m + 1, :) = VT(S0 .* u.^(0:m) .* d.^(m:-1:0), K);
-	for i = m:-1:1
-		V(i, 1:i) = (q*V(i+1, 1:i) + p*V(i+1, 2:i+1))/exp(r*dt);
+	fprintf('\nMaximum and Minimum Return Portfolio')
+	for i = 1:2
+		fprintf('\nWeights\t\t\t\t\tReturn\t\tRisk\n')
+		mu = m(i);
+		w = f(mu);
+		sig = g(mu);
+		fprintf('[')
+		for j = 1:length(w)
+			fprintf('%f, ', double(w(j)));
+		end
+		fprintf(']\t%f\t%f\n', mu, double(sig));
 	end
+end
 
-	V0 = V(1, 1);
+function q1_part4(f, g, mu)
+	fprintf('\nMinimum Risk Portfolio\nWeights\t\t\t\t\tReturn\t\tRisk\n')
+	
+	w = f(mu);
+	sig = g(mu);
+	fprintf('[')
+	for j = 1:length(w)
+		fprintf('%f, ', double(w(j)));
+	end
+	fprintf(']\t%f\t%f\n', mu, double(sig));
+end
+
+function q1_part5(f, g, h, w_m, mu_m, sig_m)
+	fprintf('\nMarket Portfolio\nWeights\t\t\t\t\tReturn\t\tRisk\n')
+	fprintf('[')
+	for j = 1:length(w_m)
+		fprintf('%f, ', double(w_m(j)));
+	end
+	fprintf(']\t%f\t%f\n', mu_m, double(sig_m));
+
+	mu = 0:0.001:0.2;
+	sig = zeros(size(mu));
+	for i = 1:length(sig)
+		sig(i) = g(mu(i));
+	end
+	% figure; plot(X, V0_call); title(['European Call price (at time 0) vs ', part]); xlabel(part); ylabel('European Call price (at time 0)');
+	% saveas(gcf, sprintf('q1_%d.png', image_num)); image_num = image_num + 1;
+	figure; plot(sig, mu); hold on;
+
+	sig = 0:0.001:0.17;
+	h(1);
+	plot(sig, h(sig));
+end
+
+function q1_part6(w_m, mu_m, sig_m)
+	fprintf('\nMixed Portfolio with Risk 10%\nWeights\t\t\t\t\tReturn\t\tRisk\n')
+	fprintf('[')
+	for j = 1:length(w_m)
+		fprintf('%f, ', double(w_m(j)));
+	end
+	fprintf(']\t%f\t%f\n', mu_m, double(sig_m));
+
+	mu = 0:0.001:0.2;
+	sig = zeros(size(mu));
+	for i = 1:length(sig)
+		sig(i) = g(mu(i));
+	end
+	% figure; plot(X, V0_call); title(['European Call price (at time 0) vs ', part]); xlabel(part); ylabel('European Call price (at time 0)');
+	% saveas(gcf, sprintf('q1_%d.png', image_num)); image_num = image_num + 1;
+	figure; plot(sig, mu); hold on;
+
+	sig = 0:0.001:0.17;
+	h(1);
+	plot(sig, h(sig));
 end
